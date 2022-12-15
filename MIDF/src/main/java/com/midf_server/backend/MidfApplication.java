d00578994d6b6989f20387e6dc278c4b4605edc0
xiuxiang wu <645698511@qq.com>
2022-12-15 19:20:03 +0800

    645698511@qq.com study2018

---
+++ MIDF/src/main/java/com/midf_server/backend/MidfApplication.java
@@ -0,0 +1,13 @@
+package com.midf_server.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MidfApplication.class, args);
+    }
+
+}
---
+++ MIDF/src/test/java/com/midf_server/backend/MidfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midf_server.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
